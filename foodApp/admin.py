5cd5af75644ad536cd42d6fbc71e2278b85aa624
1F10230183 <131662887+1F10230183@users.noreply.github.com>
2023-12-15 15:13:42 +0900

    modelsの定義とマイグレーション、フォルダの定義を行った

--- foodApp/admin.py
+++ foodApp/admin.py
@@ -1,3 +1,5 @@
+# foodApp/admin.py
 from django.contrib import admin
+from .models import Restaurant
 
-# Register your models here.
+admin.site.register(Restaurant)
--- foodApp/models.py
+++ foodApp/models.py
@@ -1,3 +1,11 @@
+# foodApp/models.py
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    cuisine_type = models.CharField(max_length=50)
+    price_range = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
